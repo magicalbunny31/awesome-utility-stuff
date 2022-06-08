3b1cd152a7ef91f74bea7e72468145959d5c9da4
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2022-06-08 16:57:23 +0100

    v1.18.5 🎉🐾

--- src/data/url.js
+++ src/data/url.js
@@ -2,4 +2,4 @@
  * my regular expression to match http(s) urls 🦊
  * @returns {RegExp} regular expression to match http(s) urls 🦊
  */
-module.exports = /https?:\/\/[-a-z0-9@:%._\+~#=]+\.[-a-z0-9@:%._\+~#=]+(\/[-a-z0-9@:%._\+~#=]+)*/gmi;
+module.exports = /^https?:\/\/[-a-z0-9@:%._\+~#=]+\.[a-z]+\b([^ ]*)$/gmi;
