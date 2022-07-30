e0e8985080560c535c167381dca0f79cd775311d
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2022-07-30 16:27:20 +0100

    v1.20.2 🎉🐾

--- src/data/emojis.js
+++ src/data/emojis.js
@@ -49,9 +49,10 @@ module.exports = {
    "category_channel": "<a:category_channel:916395583663669318>",
    "rules_channel":    "<a:rules_channel:916395583663669319>",
 
-   "shield": "<a:shield:921777486172405823>",
-   "person": "<a:person:921777485463572490>",
-   "people": "<a:people:921777485492936804>",
+   "shield":  "<a:shield:921777486172405823>",
+   "person":  "<a:person:921777485463572490>",
+   "people":  "<a:people:921777485492936804>",
+   "mention": "<a:mention:1002959964991651983>",
 
    "server_booster": "<a:server_booster:915296177799442442>",
    "wumpus_nitro":   "<a:wumpus_nitro:915297825384972288>",
