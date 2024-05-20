5307c0c0b27e7ffe298d8e46d3c0ba4d2e8865bd
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2024-05-20 21:28:42 +0100

    v1.25.10 🎉🐾

--- src/functions/formatBytes.js
+++ src/functions/formatBytes.js
@@ -11,16 +11,16 @@ module.exports = (bytes, useBytes = false, decimals = 2) => {
       return `0 ${useBytes ? `bits` : `bytes`}`;
 
    const k = useBytes
-      ? 1000
-      : 1024;
+      ? 1024
+      : 1000;
 
    const dm = decimals < 0
       ? 0
       : decimals;
 
    const sizes = useBytes
-      ? [ `bytes`, `KB`,  `MB`,  `GB`,  `TB`,  `PB`,  `EB`,  `ZB`,  `YB`  ]
-      : [ `bytes`, `KiB`, `MiB`, `GiB`, `TiB`, `PiB`, `EiB`, `ZiB`, `YiB` ];
+      ? [ `bytes`, `KiB`, `MiB`, `GiB`, `TiB`, `PiB`, `EiB`, `ZiB`, `YiB` ]
+      : [ `bytes`, `KB`,  `MB`,  `GB`,  `TB`,  `PB`,  `EB`,  `ZB`,  `YB`  ];
 
    const i = Math.floor(Math.log(bytes) / Math.log(k));
 
